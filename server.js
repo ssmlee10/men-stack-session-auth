f2c78de9d6989867ed0c6e0d9cc7649502bb2ac9
Sharon Lee <ssmlee10@gmail.com>
2025-07-22 12:21:15 -0700

    complete sign out functionality

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const session = require("express-session");
 
 // imports logic from auth.js in controllers
-const authController = require('./controllers/auth.js');
+const authController = require("./controllers/auth.js");
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -24,14 +25,21 @@ app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.get('/', async (req, res) => {
-    res.render('index.ejs');
+app.get("/", async (req, res) => {
+  res.render("index.ejs", { user: req.session.user });
 });
 
-// 
-app.use('/auth', authController);
+//
+app.use("/auth", authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
